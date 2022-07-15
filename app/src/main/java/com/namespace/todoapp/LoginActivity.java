fa38f7d2157986dac21f5d3800f071ca14d5e312
Your Name <you@example.com>
2022-07-15 17:48:20 +0530

    create and delete the TODO

--- app/src/main/java/com/namespace/todoapp/LoginActivity.java
+++ app/src/main/java/com/namespace/todoapp/LoginActivity.java
@@ -1,14 +1,12 @@
 package com.namespace.todoapp;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -85,9 +83,9 @@ public class LoginActivity extends AppCompatActivity {
 
 
     private void initializeUI() {
-        logBtn = (Button)findViewById(R.id.button2);
-        emailEditText = (EditText)findViewById(R.id.editText);
-        passwordEditText = (EditText)findViewById(R.id.editText2);
+        logBtn = findViewById(R.id.button2);
+        emailEditText = findViewById(R.id.editText);
+        passwordEditText = findViewById(R.id.editText2);
         progressBar = findViewById(R.id.progressBar);
 
     }
--- app/src/main/java/com/namespace/todoapp/MainActivity.java
+++ app/src/main/java/com/namespace/todoapp/MainActivity.java
@@ -3,14 +3,68 @@ package com.namespace.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("Try adding TODO ");
+        // Setup remove listener method call
+        setupListViewListener();
+
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    // Attaches a long click listener to the listview
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        // Remove the item within array at position
+                        items.remove(pos);
+
+                        // Refresh the adapter
+                        itemsAdapter.notifyDataSetChanged();
+                        // Return true consumes the long click event (marks it handled)
+                        return true;
+                    }
+
+                });
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+    }
+
 }
